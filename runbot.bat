48105f21234effd438122c2378f208577d98521f
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-03-06 17:54:20 +0100

    Delete runbot.bat

--- runbot.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-color 0a
-title Start Bot
-echo please enter your token
-set /k token=
-echo Connecting....
-CMD /k node bot.js
-::if you want a batch file like this then check out https://github.com/RScodes/DiscordBot-Startup-Batch-files for batch files for multiple api's
-PAUSE
