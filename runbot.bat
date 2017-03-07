8644855fed10db0ea355ecee51855eb1046d46a3
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-03-07 14:12:10 +0100

    Update runbot.bat

--- runbot.bat
+++ runbot.bat
@@ -4,6 +4,6 @@ title Start Bot
 echo please enter your token
 set /k token=
 echo Connecting....
-CMD /k node bot.js
+CMD /k node bot.js %token%
 ::if you want a batch file like this then check out https://github.com/RScodes/DiscordBot-Startup-Batch-files for batch files for multiple api's
 PAUSE
