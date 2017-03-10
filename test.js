91de4a9ff67d6b9933d7c1e73dbb83ea2099e86e
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-03-10 13:26:29 +0100

    Update test.js

--- test.js
+++ test.js
@@ -2,4 +2,14 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 const config = require("./config.json");
 
+process.on('uncaughtException', err =>{
+    console.log('error: ' + err);//STOPS THE BOT FROM CRASHING
+});
+
+bot.on('ready',() => {
+	console.log(`Connected! ${config.emojis.success}`);
+	bot.channels.get("265446863350661131").sendMessage(`${bot.user.username} test works`);
+  bot.destroy()
+});
+
 bot.login(config.token);
