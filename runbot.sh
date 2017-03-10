61f17ba463d40e4a6fa6f1699778b7ccfd5fe509
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-03-10 13:17:58 +0100

    Update runbot.sh

--- runbot.sh
+++ runbot.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+set +v echo off
+echo please enter your token
+read token
 echo Connecting....
-eval node bot.js
+eval node bot.js $token
 sleep 1000
 #if you want a batch file like this then check out https://github.com/RScodes/DiscordBot-Startup-Batch-files for batch files for multiple api's
