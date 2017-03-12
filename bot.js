e1464f047b83efb2d31b370e1b59d112210a861e
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-03-12 17:41:39 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -211,11 +211,9 @@ bot.on('message', message => { //start of command list
         	cmds += `**My Normal Commands are:** \n ${config.client.prefix}membercount \n ${config.client.prefix}serverinfo \n ${config.client.prefix}botservers \n ${config.client.prefix}date \n ${config.client.prefix}sourcecode \n ${config.client.prefix}pokemon \n ${config.client.prefix}avatar \n ${config.client.prefix}ping \n ${config.client.prefix}creator \n ${config.client.prefix}help \n ${config.client.prefix}stats \n ${config.client.prefix}myuserinfo`;
         	if(message.member.roles.has(modRole.id) || message.author.id === config.creator.Jimmy) {
         		cmds += `**My Staff commands are** \n ${config.client.prefix}embed [what you want to embed] \n ${config.client.prefix}addrole {user} [role] \n ${config.client.prefix}delrole {user} [role] \n ${config.client.prefix}announce [what you want to announce in #announcements] \n ${config.client.prefix}say [what you want to say] \n ${config.client.prefix}kick {user} \n \n more details on how to use these commands coming soon`;
-			return cmds;
         	}
         	if(message.member.roles.has(adminRole.id) || message.author.id === config.creator.Jimmy) {
         		cmds += `My Owner/Creator Commands are: \n ${config.client.prefix}setbotavatarurl (only Jimmy) \n ${config.client.prefix}setstatus (only Jimmy) \n ${config.client.prefix}shutdown \n ${config.client.prefix}restart`;
-			return cmds;
         	}
         	message.author.sendMessage(" ", {embed: {
         	    color: 0x00b7c6,
